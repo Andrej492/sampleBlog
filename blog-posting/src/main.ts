286260ca18d7a3deec92b35ba8f3e15efa5d6223
Andrej492 <andrejmatovic1@gmail.com>
2022-01-19 20:47:28 +0100

    Update: THis is a working version of only POSTS..

--- blog-posting/src/main.ts
+++ blog-posting/src/main.ts
@@ -4,10 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// import Amplify from "aws-amplify";
-// import aws_exports from "./aws-exports";
-// import { Auth } from '@aws-amplify/auth';
-// Amplify.configure(aws_exports);
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 if (environment.production) {
   enableProdMode();
