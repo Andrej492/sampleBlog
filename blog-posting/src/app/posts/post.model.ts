ce8c8c64402b7623951261f7aa58c88bfce9e349
Andrej492 <andrejmatovic1@gmail.com>
2022-01-19 08:40:55 +0100

    Update: Added comment model and updated post model

--- blog-posting/src/app/posts/post.model.ts
+++ blog-posting/src/app/posts/post.model.ts
@@ -1,5 +1,7 @@
+import { Comment } from "../shared/comment.model";
+
 export class Post {
 
-  constructor(public name: string, public description: string, public imagePath: string) {}
+  constructor(public name: string, public description: string, public imagePath: string, public comments: Comment[]) {}
 
 }
--- blog-posting/src/app/posts/post.service.ts
+++ blog-posting/src/app/posts/post.service.ts
@@ -1,27 +1,40 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Subject } from "rxjs";
+import { Comment } from "../shared/comment.model";
 import { Post } from "./post.model";
 
 @Injectable({providedIn: 'root'})
 export class PostService {
   postsChanged = new Subject<Post[]>();
-  startedEditing = new BehaviorSubject<number>(null);
+  //startedEditing = new BehaviorSubject<number>(null);
 
   private posts: Post[] = [
     new Post(
       'First Post',
       'Post about tasty Schnitzel!',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG'
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Comment('First Comment', null, null),
+        new Comment('Second comment', null, null)
+      ]
     ),
     new Post(
       'Second Post',
       'Post about big burger!',
-      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Comment('Third Comment', null, null),
+        new Comment('Fourth comment', null, null)
+      ]
     ),
     new Post(
       'Third Post',
       'Post about big burger!',
-      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Comment('Fifth Comment', null, null),
+        new Comment('Sixth comment', null, null)
+      ]
     )
   ];
 
